f904aa7d0b01be41352a2c60c4ba4f6c660ddf9e
Hannes Ljusås <hl222ih@student.lnu.se>
2013-11-15 08:25:18 +0100

    Om detta program ska kunna köras som triangel.exe behöver jag lägga till kod i Main-metoden som hanterar input och utput. Inkluderar här inputvalidering i Main.

--- WhiteBox/WhiteBox/Program.cs
+++ WhiteBox/WhiteBox/Program.cs
@@ -10,6 +10,69 @@ namespace WhiteBox
     {
         static void Main(string[] args)
         {
+            //temporärt testvärde
+            args = new string[] { "4.0", "4.0", "4.0s" };
+
+            string message = String.Empty;
+            string errorMessage = String.Empty;
+
+            if (ValidateInput(args, out errorMessage))
+            {
+            }
+            else
+            {
+                message = errorMessage;
+            }
+
+            Console.WriteLine(message);
+            Console.ReadLine();
+
+        }
+
+        private static bool ValidateInput(string[] args, out string errorMessage)
+        {
+            errorMessage = String.Empty;
+            if (args.Length != 3 && args.Length != 6)
+            {
+                errorMessage = "Felaktigt antal argument. Ange sidans längder (tre argument) eller de tre punkternas koordinater (sex argument)";
+                return false;
+            }
+
+            double[] argsAsDouble = new double[args.Length];
+            for (int i = 0; i < args.Length; i++)
+            {
+                if (!Double.TryParse(args[i], out argsAsDouble[i]))
+                {
+                    errorMessage = "Ett eller flera argument kan inte tolkas som ett flyttal (double).";
+                    return false;
+                }
+            }
+
+            if (argsAsDouble.Length == 3)
+            {
+                foreach (double argAsDouble in argsAsDouble)
+                {
+                    if (argAsDouble <= 0 || argAsDouble == Double.PositiveInfinity)
+                    {
+                        errorMessage = "Ett eller flera argument representerar för stora tal eller för små tal.";
+                        return false;
+                    }
+                }
+            }
+
+            if (argsAsDouble.Length == 6)
+            {
+                foreach (double argAsDouble in argsAsDouble)
+                {
+                    if (Double.IsInfinity(argAsDouble))
+                    {
+                        errorMessage = "Ett eller flera argument representerar för stora eller för små tal.";
+                        return false;
+                    }
+                }
+            }
+
+            return true;
         }
     }
 }
